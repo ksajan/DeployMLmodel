7acd3f5e4af1cbc9a119771ff1161f90e3a4c308
Sajan <ksajan12@gmail.com>
2020-03-18 05:33:47 -0700

    working html file

--- SpamDetector/app.py
+++ SpamDetector/app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 #home directory file to render index html file
 @app.route('/')
 def home():
-    return "Testing debug option."         #render_template('home.html')
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
