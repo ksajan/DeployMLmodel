cc8e5a715fbf1baecdde16da53db4547d31479ee
Sajan <ksajan12@gmail.com>
2020-03-18 05:05:59 -0700

    updated flask file

--- SpamDetector/app.py
+++ SpamDetector/app.py
@@ -1,1 +1,16 @@
+#importing necessary libraries
 import numpy as np
+import pandas as pd
+from flask import Flask, request, render_template, url_for
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.externals import joblib
+
+# Iitiated flask instance
+app = (__Flask__)
+
+#home directory file to render index html file
+@app.route('/')
+def home():
+    return render_template('home.html')
