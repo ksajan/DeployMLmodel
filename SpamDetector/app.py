9bcde75bd65a4ccfa4550f4f051d52304e48efae
Sajan <ksajan12@gmail.com>
2020-03-21 21:20:24 -0700

    added result html

--- SpamDetector/app.py
+++ SpamDetector/app.py
@@ -14,6 +14,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
-
+@app.route('/')
+def result():
+    return render_template('result.html')
 if __name__ == '__main__':
     app.run(debug=True)
